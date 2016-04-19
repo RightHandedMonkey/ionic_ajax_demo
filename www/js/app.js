75939aeb2054e592f2a87257bed1c6350898a77c
RightHandedMonkey <rooster@richcap.com>
2016-04-19 12:34:00 -0400

    Added method to serialize the data to a standard POST x-www-form-urlencoded format

--- www/js/app.js
+++ www/js/app.js
@@ -6,88 +6,115 @@
 angular.module('starter', ['ionic'])
 
 .run(function($ionicPlatform) {
-        $ionicPlatform.ready(function() {
-            if (window.cordova && window.cordova.plugins.Keyboard) {
-                // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-                // for form inputs)
-                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    $ionicPlatform.ready(function() {
+      if (window.cordova && window.cordova.plugins.Keyboard) {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(
+          true);
 
-                // Don't remove this line unless you know what you are doing. It stops the viewport
-                // from snapping when text inputs are focused. Ionic handles this internally for
-                // a much nicer keyboard experience.
-                cordova.plugins.Keyboard.disableScroll(true);
-            }
-            if (window.StatusBar) {
-                StatusBar.styleDefault();
-            }
+        // Don't remove this line unless you know what you are doing. It stops the viewport
+        // from snapping when text inputs are focused. Ionic handles this internally for
+        // a much nicer keyboard experience.
+        cordova.plugins.Keyboard.disableScroll(true);
+      }
+      if (window.StatusBar) {
+        StatusBar.styleDefault();
+      }
+    });
+  })
+  .controller('AjaxCtrl', function($scope, $http) {
+
+    $scope.url = "http://espn.go.com";
+
+    var clearMessages = function() {
+      $scope.message = "";
+      $scope.error_msg = "";
+    }
+
+    $scope.makeNetworkGetCall = function() {
+      console.log("Network GET call");
+      http_get($scope, $http, $scope.url);
+    };
+
+    $scope.makeNetworkPostCall = function() {
+      console.log("Network POST call");
+      console.log($scope.url);
+      http_post($scope, $http, $scope.url, {
+        "data1": "bitter",
+        "data2": "sweet"
+      });
+    };
+
+    var http_get = function($scope, $http, url) {
+      clearMessages();
+      $http.get(url)
+        .success(function(data, status, headers, config) {
+          $scope.message = data;
+          console.log("Successful call to " + url);
+        }).error(function(data, status, headers, config) {
+          showConnectionError();
+          logall(data, status, headers, config);
         });
-    })
-    .controller('AjaxCtrl', function($scope, $http) {
 
-        $scope.url = "http://espn.go.com";
+    };
 
-        var clearMessages = function() {
-            $scope.message = "";
-            $scope.error_msg = "";
+    var http_post = function($scope, $http, url, postdata) {
+      clearMessages();
+      $http({
+        url: url,
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        data: serializeData(postdata)
+      }).success(function(data, status, headers, config) {
+        $scope.message = data;
+        console.log("Successful call to " + url);
+      }).error(function(data, status, headers, config) {
+        showConnectionError();
+        logall(data, status, headers, config);
+      });
+    };
+    //FROM: http://www.bennadel.com/blog/2615-posting-form-data-with-http-in-angularjs.htm
+    function serializeData(data) {
+      // If this is not an object, defer to native stringification.
+      if (!angular.isObject(data)) {
+        return ((data == null) ? "" : data.toString());
+      }
+      var buffer = [];
+      // Serialize each key in the object.
+      for (var name in data) {
+        if (!data.hasOwnProperty(name)) {
+          continue;
         }
+        var value = data[name];
+        buffer.push(
+          encodeURIComponent(name) +
+          "=" +
+          encodeURIComponent((value == null) ? "" : value)
+        );
+      }
+      // Serialize the buffer and clean it up for transportation.
+      var source = buffer
+        .join("&")
+        .replace(/%20/g, "+");
+      return (source);
+    }
 
-        $scope.makeNetworkGetCall = function() {
-            console.log("Network GET call");
-            http_get($scope, $http, $scope.url);
-        };
+    var logall = function(data, status, headers, config) {
+      console.log("data: " + data);
+      console.log("status: " + status);
+      console.log("headers: " + headers);
+      console.log("config: ");
+      console.log(config);
+      console.log("error: " + status);
+    }
 
-        $scope.makeNetworkPostCall = function() {
-            console.log("Network POST call");
-            console.log($scope.url);
-            http_post($scope, $http, $scope.url, {
-                "data1": "bitter",
-                "data2": "sweet"
-            });
-        };
+    var showConnectionError = function() {
+      $scope.error_msg =
+        "Could not connect to server - check for missing 'http' in URL.  Check console for Cross-Origin Request Blocked message.";
 
-        var http_get = function($scope, $http, url) {
-            clearMessages();
-            $http.get(url)
-                .success(function(data, status, headers, config) {
-                    $scope.message = data;
-                    console.log("Successful call to " + url);
-                }).error(function(data, status, headers, config) {
-                    showConnectionError();
-                    logall(data, status, headers, config);
-                });
+    }
 
-        };
-
-        var http_post = function($scope, $http, url, postdata) {
-            clearMessages();
-            $http({
-                url: url,
-                method: "POST",
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded'
-                },
-                data: postdata
-            }).success(function(data, status, headers, config) {
-                $scope.message = data;
-                console.log("Successful call to " + url);
-            }).error(function(data, status, headers, config) {
-                showConnectionError();
-                logall(data, status, headers, config);
-            });
-        };
-
-        var logall = function(data, status, headers, config) {
-            console.log("data: " + data);
-            console.log("status: " + status);
-            console.log("headers: " + headers);
-            console.log("config: ");
-            console.log(config);
-            console.log("error: " + status);
-        }
-
-        var showConnectionError = function() {
-            $scope.error_msg = "Could not connect to server - check for missing 'http' in URL.  Check console for Cross-Origin Request Blocked message.";
-
-        }
-
-    })
+  })
